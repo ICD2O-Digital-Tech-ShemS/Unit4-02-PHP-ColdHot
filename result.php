878bbf85fdb8fe817801948068531bda190a1562
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-05 14:57:03 +0000

    Update 5 Assign-03

--- result.php
+++ result.php
@@ -5,9 +5,9 @@
         if ($Final >= 15) {
             echo "It is Hot out side";
         }
-        if  ($Final <= 15) {
+        else  ($Final <= 15) {
             echo  "It is cold"; 
     }
 ?>
 <h3>Results:</h3>
-Volume of a sphere is <?php echo "$Final" ?>°F.
+    <?php echo "$Final" ?>°F.
